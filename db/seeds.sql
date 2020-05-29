37c18f3f2a54d6eaae66bcb47939d72d918f8ce5
shuo <shuo.li88@hotmail.com>
2020-05-29 15:30:34 +0800

    first upload

---
+++ db/seeds.sql
@@ -0,0 +1,3 @@
+INSERT INTO burgers (burger_name) VALUES ('Cheeseburger');
+INSERT INTO burgers (burger_name) VALUES ('Double bacon cheeseburger');
+INSERT INTO burgers (burger_name) VALUES ('Lamb burger');
